ed0d92258ea55906faf46e0fcca11b845532d877
Noni Porter <n@nonis-air.mynetworksettings.com>
2024-01-02 19:34:50 -0500

    working addition and multiplication counter with a reset button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const addOne = () => {
+    setCount(count + 1);
+  }
+
+  const doubleTheNumber = () => {
+    setCount(count * 2);
+  }
+
+  const resetCount = () => {
+    setCount(0)
+  }
+
+
   return (
     <div className="App">
       <div className="number">{count}</div>
       <div className="buttons-container">
-        <div className="counter-button">+1</div>
-        <div className="counter-button blue">x2</div>
+        <div className="counter-button" onClick={addOne}>+1</div>
+        <div className="counter-button blue" onClick={doubleTheNumber}>x2</div>
+        <div className="reset-button" onClick={resetCount}>Resest</div>
       </div>
     </div>
   );
